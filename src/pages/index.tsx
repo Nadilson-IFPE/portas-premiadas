b536f4f6984b1a7cd3e64147aea76e9a3762bdb4
Nadilson <nadilson@protonmail.com>
2023-03-07 19:59:56 -0300

    Adicionando eventos à porta e modificações no CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,13 @@ import { Inter } from 'next/font/google'
 import Presente from '@/components/Presente'
 import Porta from '@/components/Porta'
 import PortaModel from '@/model/portamodel'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const p1 = new PortaModel(1, false, true)
-  const p2 = new PortaModel(2)
-  const p3 = new PortaModel(3)
-  const p4 = new PortaModel(4)
-  const p5 = new PortaModel(5)
-  const p6 = new PortaModel(6)
+  const [p1, setP1] = useState(new PortaModel(1))
+
 
   return (
     <>
@@ -23,13 +20,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div style={{ display: "flex" }}>
-        <Porta porta={p1} />
-        <Porta porta={p2} />
-        <Porta porta={p3} />
-        <Porta porta={p4} />
-        <Porta porta={p5} />
-        <Porta porta={p6} />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
       </div>
 
     </>
